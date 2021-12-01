859507be52c1edca45fc82758cc39cf083a2bdea
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2021-12-01 00:38:47 -0500

    day 1 solved

---
+++ Day1/day1_puzzle2.py
@@ -0,0 +1,18 @@
+def myPuzzle(scanning:list):
+    cont = 0
+    for i in range(3,len(scanning)):
+        if scanning[i] > scanning[i-3]:
+            cont+=1
+    return cont
+    
+
+def getList():
+    with open("Day1\day1_puzzle1_list.txt") as f:
+        return [int(line.strip()) for line in f]
+
+#Test case
+aux = [199,200,208,210,200,207,240,269,260,263]
+print(myPuzzle(aux))
+
+#Print solution
+print(myPuzzle(getList()))
